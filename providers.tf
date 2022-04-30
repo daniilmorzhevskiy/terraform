d0407314e1eeea76294524f72d2770bb8bf7a271
Dan <88776658+daniilmorzhevksiy@users.noreply.github.com>
2022-04-30 15:17:12 +0300

    added some files

---
+++ providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  shared_credentials_file = "~/.aws/creds"
+  profile                 = "vscode"
+  region                  = "us-east-1"
+}   
